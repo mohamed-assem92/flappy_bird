7d58cd5019b218fd69d8f8981ea5d2c5cb19554a
“Ahmed <mardwow2012@gmail.com>
2018-01-12 18:38:17 +0200

    new update on envitonment class

--- pro1.js
+++ pro1.js
@@ -15,11 +15,18 @@ var showscore = document.getElementById('showscore');
 var playagainbtn = document.getElementById('playagain');
 var obsimage = document.getElementById('obsimage');
 var imagesarray = ["images/obs2.png","images/obs1.png"];
-var heightarray = ["150","200","250","300","350","400","500","550"];
+var heightarray = ["70","90","110","130","150","170","190","210,230,300"];
+var level = document.getElementById("level")
+var choosebtn = document.getElementById("choose")
+var levelsimg = document.getElementById("levels")
+var showlevel = document.getElementById("showlevel")
+var level_1_btn = document.getElementById("choose1")
+var level_2_btn = document.getElementById("choose2")
+var level_3_btn = document.getElementById("choose3")
 
 
-img.width = 100;
-img.height = 80;
+img.width = 80;
+img.height = 60;
 var birdwings = [];
 var count = 0;
 var keepjump = 'true';
@@ -27,14 +34,14 @@ var crash = 'false';
 
 //##### the bird class #####
  var bird = function() {
-   this.pos_y = 100;
-   this.pos_x = 150;
+   this.pos_y = 20;
+   this.pos_x = 20;
    this.player_name;
    this.lives = 3;
    this.scoree = 0;
    window.addEventListener('keyup', function(event) {
-     if (event.keyCode === 32 && this.pos_y > 10 && keepjump === 'true') {
-       this.pos_y -= 40;
+     if (event.keyCode === 32 && this.pos_y > 50 && keepjump === 'true') {
+       this.pos_y -= 35;
        img.style.top= this.pos_y +'px';
      }
    }.bind(this))
@@ -83,9 +90,9 @@ bird.prototype.changeimg = function () {
   }
 };
 bird.prototype.crash = function () {
-  if (parseInt(getComputedStyle(obsimage).left) <= this.pos_x) {
-    if (this.pos_y+50 >= parseInt(getComputedStyle(obsimage).top)) {
-      this.pos_y = 100;
+  if (parseInt(getComputedStyle(obsimage).left)-50 <= this.pos_x) {
+    if (this.pos_y+20 >= parseInt(getComputedStyle(obsimage).top)) {
+      this.pos_y = 20;
       this.lives--;
       displaylives1.innerHTML = ` x${this.lives}`;
       this.whencrash();
@@ -110,8 +117,8 @@ bird.prototype.set_y = function () {
   displaylives.src = birdwings[0];
   displaylives1.innerHTML = ` x${this.lives}`;
   this.crash();
-  if (this.pos_y > window.innerHeight-120 && this.lives > 0) {
-    this.pos_y = 100;
+  if (this.pos_y > 500 && this.lives > 0) {
+    this.pos_y = 20;
     this.lives--;
     displaylives1.innerHTML = ` x${this.lives}`;
     this.whencrash();
@@ -130,25 +137,69 @@ bird.prototype.set_y = function () {
 
 
 playbutton.addEventListener("click", function() {
-  if (character[0].checked === true) {
-    birdwings = ["images/1.png","images/2.png","images/3.png","images/4.png"];
-  }
-  if (character[1].checked === true) {
-    birdwings = ["images/11.png","images/22.png","images/33.png","images/44.png"];
-  }
-  if (character[0].checked === false && character[1].checked === false) {
-    birdwings = ["images/111.png","images/222.png","images/333.png","images/444.png"];
-  }
+  images();
   startpage.style.display = 'none';
   gamepage.style.display = 'block';
   gameloop();
 })
+/////imgaes/////////////////
+function images() {
+    if (character[0].checked === true) {
+        birdwings = ["images/1.png","images/2.png","images/3.png","images/4.png"];
+    }
+    if (character[1].checked === true) {
+        birdwings = ["images/11.png","images/22.png","images/33.png","images/44.png"];
+    }
+    if (character[0].checked === false && character[1].checked === false) {
+        birdwings = ["images/111.png","images/222.png","images/333.png","images/444.png"];
+    }
+}
+
+////////////////////levels////////////
+    showlevel.addEventListener("click", function() {
+    startpage.style.display = 'none';
+    level.style.display = 'block';ء
+})
+
+level_1_btn.addEventListener("click" , function () {
+    images();
+    level.style.display = 'none';
+    gamepage.style.backgroundImage = "url('images/12.png')";
+    gamepage.style.display = 'block';
+    gameloop();
+
+});
+
+level_2_btn.addEventListener("click" , function () {
+    images();
+    level.style.display = 'none';
+    gamepage.style.backgroundImage = "url('images/2.jpg')";
+    gamepage.style.display = 'block';
+    gameloop();
+
+});
+
+level_3_btn.addEventListener("click" , function () {
+    images();
+    level.style.display = 'none';
+    gamepage.style.backgroundImage = "url('images/3.jpg')";
+    gamepage.style.display = 'block';
+    gameloop();
+
+});
+
 
 //###### the game loop ######
 function gameloop() {
   var b1 = new bird;
   var obs = new obstacles;
   obs.obsmove();
+
+  ////
+    if(b1.scoree === 4)
+    {
+        gamepage.style.backgroundImage = "url('images/12.png')";
+    }
 
   b1.setplayername(playername.value);
   var printname = b1.getplayername();
@@ -158,10 +209,11 @@ function gameloop() {
   b1.gravity();
   //b1.birdjump();
   b1.printscore();
-
+  const Env = new Environment()
+  Env.movesky();
   playagain.addEventListener("click", function() {
     b1.scoree = 0;
-    b1.pos_y = 100;
+    b1.pos_y = 20;
     b1.lives = 3;
     b1.wingmove();
     b1.gravity();
@@ -171,6 +223,7 @@ function gameloop() {
     endpage.style.display = 'none';
   })
 }
+////////////fff////////score
 
 
 
@@ -179,14 +232,14 @@ obsimage.src = imagesarray[Math.floor(Math.random() * imagesarray.length)];
 obsimage.height = heightarray[Math.floor(Math.random() * heightarray.length)];
 
 var obstacles = function() {
-  this.posx = 10;
+  this.posx = -50;
   this.posy = 0;
 }
 obstacles.prototype.setposx = function () {
-  this.posx += 10;
+  this.posx += 5;
   obsimage.style.right = this.posx +"px";
-  if (this.posx > window.innerWidth-140) {
-    this.posx = 10;
+  if (this.posx >= 800) {
+    this.posx = -50;
     setTimeout(function() {
       obsimage.src = imagesarray[Math.floor(Math.random() * imagesarray.length)];
       obsimage.height = heightarray[Math.floor(Math.random() * heightarray.length)];
@@ -205,3 +258,37 @@ obstacles.prototype.getposy = function () {
 obstacles.prototype.obsmove = function () {
   moveleft = setInterval(this.setposx.bind(this), 50);
 };
+///////////environment
+const  Environment = function () {
+    this.currentpos = 0 ;
+};
+Environment.prototype.moveground = function()
+{
+    this.currentpos -= 2 ;
+    gamepage.style.backgroundPositionX =  this.currentpos + "px"
+
+};
+Environment.prototype.movesky = function () {
+    var i = setInterval(this.moveground.bind(this) ,50)
+
+};
+
+
+function loadingpage() {
+  var bar = document.getElementById('progress-bar')
+  var prog = document.getElementById('progress');
+  var progress = 25;
+  loadinginterval = setInterval(function() {
+    if (progress === 500) {
+      clearInterval(loadinginterval);
+      bar.style.display = "none"
+      startpage.style.display = "block"
+    }
+    else {
+      progress += 5;
+      prog.style.width = progress + 'px';
+    }
+  },10)
+}
+
+loadingpage();
